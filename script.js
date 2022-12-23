9b404a37bea80af3166ac0f11a38034bcd6aab55
NickFler <nikita.fler0603@gmail.com>
2022-12-23 19:02:52 +0200

    nick

---
+++ script.js
@@ -0,0 +1,7 @@
+function navToggle() {
+    var navToggleNav = document.getElementById("nav");
+    var html = document.getElementById("html");
+    navToggleNav.classList.toggle("_active");
+   html.classList.toggle("_overFlow");
+    
+}
